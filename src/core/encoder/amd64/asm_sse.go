cb66b21795a6def068bc26160825cd3aa98338ea
jtenner <3761339+jtenner@users.noreply.github.com>
2026-07-03 21:44:23 +0000

    amd64: add packed unsigned minmax encoders
    Why:
    - SIMD unsigned min/max lowering needs x86-only VEX.128 helpers for byte, word, and dword lanes.
    
    What:
    - Add VPminub/VPmaxub, VPminuw/VPmaxuw, and VPminud/VPmaxud helpers.
    - Golden-test normal and high-XMM encodings for each helper.
    
    Proof:
    - go test ./src/core/encoder/amd64
    
    Docs:
    - unchanged, amd64 encoder remains Wasm-agnostic and existing baseline docs already cover SSE4.1 plus AVX/VEX.128.
    
    Signed-off-by: jtenner <3761339+jtenner@users.noreply.github.com>

--- src/core/encoder/amd64/asm_sse.go
+++ src/core/encoder/amd64/asm_sse.go
@@ -264,11 +264,17 @@ func (a *Asm) VPadddMemDisp(dst, s1, base Reg, disp int32) {
 func (a *Asm) VPshufb(dst, s1, s2 Reg) { a.vex3RRRMap(vexMap0F38, 0b01, 0x00, dst, s1, s2) }
 func (a *Asm) VPmullw(dst, s1, s2 Reg) { a.vex3RRR(0b01, 0xD5, dst, s1, s2) }
 func (a *Asm) VPminsb(dst, s1, s2 Reg) { a.vex3RRRMap(vexMap0F38, 0b01, 0x38, dst, s1, s2) }
+func (a *Asm) VPminub(dst, s1, s2 Reg) { a.vex3RRR(0b01, 0xDA, dst, s1, s2) }
 func (a *Asm) VPmaxsb(dst, s1, s2 Reg) { a.vex3RRRMap(vexMap0F38, 0b01, 0x3C, dst, s1, s2) }
+func (a *Asm) VPmaxub(dst, s1, s2 Reg) { a.vex3RRR(0b01, 0xDE, dst, s1, s2) }
 func (a *Asm) VPminsw(dst, s1, s2 Reg) { a.vex3RRR(0b01, 0xEA, dst, s1, s2) }
+func (a *Asm) VPminuw(dst, s1, s2 Reg) { a.vex3RRRMap(vexMap0F38, 0b01, 0x3A, dst, s1, s2) }
 func (a *Asm) VPmaxsw(dst, s1, s2 Reg) { a.vex3RRR(0b01, 0xEE, dst, s1, s2) }
+func (a *Asm) VPmaxuw(dst, s1, s2 Reg) { a.vex3RRRMap(vexMap0F38, 0b01, 0x3E, dst, s1, s2) }
 func (a *Asm) VPminsd(dst, s1, s2 Reg) { a.vex3RRRMap(vexMap0F38, 0b01, 0x39, dst, s1, s2) }
+func (a *Asm) VPminud(dst, s1, s2 Reg) { a.vex3RRRMap(vexMap0F38, 0b01, 0x3B, dst, s1, s2) }
 func (a *Asm) VPmaxsd(dst, s1, s2 Reg) { a.vex3RRRMap(vexMap0F38, 0b01, 0x3D, dst, s1, s2) }
+func (a *Asm) VPmaxud(dst, s1, s2 Reg) { a.vex3RRRMap(vexMap0F38, 0b01, 0x3F, dst, s1, s2) }
 func (a *Asm) VPshufbMemIdx(dst, s1, base, index Reg, disp int32) {
 	a.vex3MemIdx(vexMap0F38, 0b01, 0x00, dst, s1, true, base, index, disp)
 }
