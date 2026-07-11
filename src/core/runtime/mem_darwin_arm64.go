81c6b98188d5839bceb76a6decae5156aa56913f
Jairus <me@jairus.dev>
2026-07-10 21:33:05 -0700

    ci: test native OS and architecture matrix (#239)

--- src/core/runtime/mem_darwin_arm64.go
+++ src/core/runtime/mem_darwin_arm64.go
@@ -5,27 +5,9 @@ package runtime
 import (
 	"sync"
 	"syscall"
-	"unsafe"
 )
 
 const pageSize = 16 << 10
-
-const madvZero = 11
-
-func madviseDontNeed(b []byte) error {
-	if len(b) == 0 {
-		return nil
-	}
-	if _, _, errno := syscall.Syscall(syscall.SYS_MADVISE,
-		uintptr(unsafe.Pointer(&b[0])), uintptr(len(b)), madvZero); errno == 0 {
-		return nil
-	}
-	if _, _, errno := syscall.Syscall(syscall.SYS_MADVISE,
-		uintptr(unsafe.Pointer(&b[0])), uintptr(len(b)), syscall.MADV_DONTNEED); errno != 0 {
-		return errno
-	}
-	return nil
-}
 
 func roundUpPage(n int) int {
 	if n <= 0 {
