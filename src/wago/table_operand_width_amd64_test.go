e1de737e12fe6e2c1e0bf639a027b9d4e60a3f61
jtenner <3761339+jtenner@users.noreply.github.com>
2026-07-17 01:01:36 +0000

    test: cover dirty table copy and init lengths

--- src/wago/table_operand_width_amd64_test.go
+++ src/wago/table_operand_width_amd64_test.go
@@ -31,14 +31,19 @@ func dirtyTableOperationsModule() []byte {
 	sig := wasmtest.FuncType(nil, []wasm.ValType{wasm.I32})
 	imp := append(append(wasmtest.Name("env"), wasmtest.Name("index")...), 0x00, 0x00)
 	table := wasmtest.Section(4, wasmtest.Vec([]byte{0x70, 0x01, 0x02, 0x04}))
-	// Active element at slot 1 containing local target function index 1.
-	elem := wasmtest.Section(9, wasmtest.Vec([]byte{0x00, 0x41, 0x01, 0x0b, 0x01, 0x01}))
+	// Active element at slot 1 plus a passive segment containing target function 1.
+	elem := wasmtest.Section(9, wasmtest.Vec(
+		[]byte{0x00, 0x41, 0x01, 0x0b, 0x01, 0x01},
+		[]byte{0x01, 0x00, 0x01, 0x01},
+	))
 	bodies := [][]byte{
 		{0x41, 0x2a, 0x0b},                   // target => 42
 		{0x10, 0x00, 0x25, 0x00, 0xd1, 0x0b}, // get => ref.is_null
-		{0x10, 0x00, 0xd0, 0x70, 0x26, 0x00, 0x41, 0x01, 0x25, 0x00, 0xd1, 0x0b},                   // set then read slot 1
-		{0x10, 0x00, 0xd0, 0x70, 0x41, 0x01, 0xfc, 0x11, 0x00, 0x41, 0x01, 0x25, 0x00, 0xd1, 0x0b}, // fill one then read
-		{0x10, 0x00, 0x11, 0x00, 0x00, 0x0b},                                                       // call_indirect
+		{0x10, 0x00, 0xd0, 0x70, 0x26, 0x00, 0x41, 0x01, 0x25, 0x00, 0xd1, 0x0b},                         // set then read slot 1
+		{0x10, 0x00, 0xd0, 0x70, 0x41, 0x01, 0xfc, 0x11, 0x00, 0x41, 0x01, 0x25, 0x00, 0xd1, 0x0b},       // fill one then read
+		{0x10, 0x00, 0x11, 0x00, 0x00, 0x0b},                                                             // call_indirect
+		{0x41, 0x00, 0x41, 0x01, 0x10, 0x00, 0xfc, 0x0e, 0x00, 0x00, 0x41, 0x00, 0x25, 0x00, 0xd1, 0x0b}, // table.copy length
+		{0x41, 0x00, 0x41, 0x00, 0x10, 0x00, 0xfc, 0x0c, 0x01, 0x00, 0x41, 0x00, 0x25, 0x00, 0xd1, 0x0b}, // table.init length
 	}
 	codes := make([][]byte, len(bodies))
 	for i := range bodies {
@@ -47,13 +52,15 @@ func dirtyTableOperationsModule() []byte {
 	return wasmtest.Module(
 		wasmtest.Section(1, wasmtest.Vec(sig)),
 		wasmtest.Section(2, wasmtest.Vec(imp)),
-		wasmtest.Section(3, wasmtest.Vec(wasmtest.ULEB(0), wasmtest.ULEB(0), wasmtest.ULEB(0), wasmtest.ULEB(0), wasmtest.ULEB(0))),
+		wasmtest.Section(3, wasmtest.Vec(wasmtest.ULEB(0), wasmtest.ULEB(0), wasmtest.ULEB(0), wasmtest.ULEB(0), wasmtest.ULEB(0), wasmtest.ULEB(0), wasmtest.ULEB(0))),
 		table,
 		wasmtest.Section(7, wasmtest.Vec(
 			wasmtest.ExportEntry("get", 0, 2),
 			wasmtest.ExportEntry("set", 0, 3),
 			wasmtest.ExportEntry("fill", 0, 4),
 			wasmtest.ExportEntry("indirect", 0, 5),
+			wasmtest.ExportEntry("copy", 0, 6),
+			wasmtest.ExportEntry("init", 0, 7),
 		)),
 		elem,
 		wasmtest.Section(10, wasmtest.Vec(codes...)),
@@ -71,6 +78,8 @@ func TestTable32OperationsCanonicalizeDirtySynchronousHostResult(t *testing.T) {
 		{name: "set", want: 1},
 		{name: "fill", want: 1},
 		{name: "indirect", want: 42},
+		{name: "copy", want: 0},
+		{name: "init", want: 0},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			in, err := Instantiate(compiled, InstantiateOptions{Imports: Imports{"env.index": HostFunc(func(_ HostModule, _, results []uint64) {
