e1f1057cc917b9e561fa9716976398dd940d78df
jtenner <3761339+jtenner@users.noreply.github.com>
2026-07-17 00:47:31 +0000

    test: cover numeric GC array bulk widths

--- src/core/runtime/gc/array_bulk_bench_test.go
+++ src/core/runtime/gc/array_bulk_bench_test.go
@@ -10,7 +10,8 @@ func newBulkBenchmarkCollector(b *testing.B) *Collector {
 	i8, _ := NewArrayDesc(1, StorageI8)
 	refs, _ := NewArrayDesc(2, StorageRef)
 	nullable, _ := NewArrayDesc(3, StorageRefNull)
-	c, err := NewCollector(Config{}, []TypeDesc{obj, i8, refs, nullable})
+	i64, _ := NewArrayDesc(4, StorageI64)
+	c, err := NewCollector(Config{}, []TypeDesc{obj, i8, refs, nullable, i64})
 	if err != nil {
 		b.Fatal(err)
 	}
@@ -29,6 +30,32 @@ func BenchmarkArrayBulk(b *testing.B) {
 			b.ResetTimer()
 			for i := 0; i < b.N; i++ {
 				if err := c.ArrayCopy(dst, 0, src, 0, n); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+		b.Run("init-data-"+benchmarkLength(n), func(b *testing.B) {
+			c := newBulkBenchmarkCollector(b)
+			dst, _ := c.NewArrayDefault(1, n)
+			data := make([]byte, n)
+			b.ReportAllocs()
+			b.SetBytes(int64(n))
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				if err := c.ArrayInitData(dst, 0, data, 0, n); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+		b.Run("init-words-"+benchmarkLength(n), func(b *testing.B) {
+			c := newBulkBenchmarkCollector(b)
+			dst, _ := c.NewArrayDefault(4, n)
+			words := make([]uint64, n)
+			b.ReportAllocs()
+			b.SetBytes(int64(n * 8))
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				if err := c.ArrayInitWords(dst, 0, words); err != nil {
 					b.Fatal(err)
 				}
 			}
