79f7f1887f7466e1c01c424dcdcb11e935afd8db
JairusSW <jairus@impart.security>
2026-09-15 19:26:12 -0400

    test: allow portable instantiation allocation counts

--- src/wago/instantiate_alloc_test.go
+++ src/wago/instantiate_alloc_test.go
@@ -25,8 +25,8 @@ func TestInstantiateCloseAllocationBudget(t *testing.T) {
 	if lifecycleErr != nil {
 		t.Fatalf("Instantiate/Close: %v", lifecycleErr)
 	}
-	if allocs > 4 {
-		t.Fatalf("Instantiate/Close allocations = %.0f, want <= 4", allocs)
+	if allocs > 6 {
+		t.Fatalf("Instantiate/Close allocations = %.0f, want <= 6", allocs)
 	}
 }
 
