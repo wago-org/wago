cb46ba3906b9d8d4dc701b8787134b3a7c0d0f5c
JairusSW <jairus@impart.security>
2026-09-24 07:52:20 -0400

    wago: eliminate repeated narrow-slot copy bounds checks

--- src/wago/scalar_slots_copy_go.go
+++ src/wago/scalar_slots_copy_go.go
@@ -3,8 +3,11 @@
 package wago
 
 func copyNarrowScalarSlots(dst, src []uint64, n int) {
+	dst, src = dst[:n], src[:n]
 	i := 0
 	for ; i+4 <= n; i += 4 {
+		_ = dst[i+3]
+		_ = src[i+3]
 		dst[i] = uint64(uint32(src[i]))
 		dst[i+1] = uint64(uint32(src[i+1]))
 		dst[i+2] = uint64(uint32(src[i+2]))
